65deda80803eefc531d7967ebf6ee3c01e536234
rakhmarufaidahanum <32060625+rakhmarufaidahanum@users.noreply.github.com>
2018-05-05 15:33:15 +0700

    bikin model dan migration

---
+++ database/migrations/2018_05_05_081102_pengguna_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PenggunaTable extends Migration
+{
+     public $tablename = 'pengguna';
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::create($this->tablename, function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('username_pengguna')->unique();
+            $table->string('nama_pengguna');
+            $table->string('alamat_pengguna');
+            $table->string('nohp_pengguna')->unique();
+            $table->string('email_pengguna')->unique();
+            $table->string('password_pengguna');
+
+            $table->timestamps();
+
+
+       }); 
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->tablename);
+    }
+}
